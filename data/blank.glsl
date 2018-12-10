61a58d842e4f3388be976d32c7bc4be7aa325874
Nick Fox-Gieg <nick@fox-gieg.com>
2018-12-09 21:08:50 -0500

    Update blank.glsl

--- data/blank.glsl
+++ data/blank.glsl
@@ -4,8 +4,9 @@ uniform sampler2D texture;
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
 	vec2 uv = fragCoord.xy / iResolution.xy;
+	vec2 uv2 = vec2(uv.x, abs(1.0 - uv.y));
 
-	vec4 col = texture2D(texture, uv);
+	vec4 col = texture2D(texture, uv2);
 
 	fragColor = col;
 }
